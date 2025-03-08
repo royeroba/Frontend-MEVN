e6586c4cca26bcf59cd774f12ad0df58e3f29227
Anibal Royero <132408372+royeroba@users.noreply.github.com>
2025-03-08 10:37:25 -0500

    fix responsive design

--- src/views/appointments/ServicesView.vue
+++ src/views/appointments/ServicesView.vue
@@ -1,19 +1,20 @@
 <script setup>
-    import ServiceItem from '../../components/ServiceItem.vue'
-    import {useServicesStore} from '../../stores/services'
-    const store = useServicesStore()
+import ServiceItem from "../../components/ServiceItem.vue";
+import { useServicesStore } from "../../stores/services";
+const store = useServicesStore();
 </script>
 
 <template>
-    <h2 class="text-4xl font-extrabold text-white mt-10">Servicios</h2>
-    <p class="text-white text-lg mt-5">A continuación elige al menos un servicio para tu cita</p>
+  <h2 class="text-4xl font-extrabold text-white mt-10">Servicios</h2>
+  <p class="text-white text-lg mt-5">
+    A continuación elige al menos un servicio para tu cita
+  </p>
 
-    <div class="grid grid-cols-2 gap-5 mt-5">
-        <ServiceItem
-            v-for="service in store.services"
-            :key="service._id"
-            :service="service"
-        />
-    </div>
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-5 mt-5">
+    <ServiceItem
+      v-for="service in store.services"
+      :key="service._id"
+      :service="service"
+    />
+  </div>
 </template>
-
